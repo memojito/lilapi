f211bb53f54766f8f27e778dbbc0d87b4daa1837
paulhenke <paul.henke@helloclue.com>
2023-05-01 20:17:21 +0200

    polishing

--- server/main.go
+++ server/main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"flag"
-	"github.com/memojito/lilapi/api/endpoints"
-	"github.com/memojito/lilapi/db"
 	"log"
 	"net/http"
+
+	"github.com/memojito/lilapi/api/endpoints"
+	"github.com/memojito/lilapi/db"
 )
 
 func main() {
 	address := flag.String("address", "localhost:8080", "The address to listen to")
+	flag.Parse()
 
 	session, err := db.NewSession()
 	if err != nil {
-		log.Printf("Connection failed")
+		log.Printf("Connection failed %v", err)
+		return
 	}
 
 	api := endpoints.NewAPI(session)
 
+	log.Print("Starting listen on:", *address)
 	http.ListenAndServe(*address, api)
 }
